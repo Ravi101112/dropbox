ae7a11175ca23aaa0d432829bae5c2dab69ef6a7
Ravi verma <103815421+Ravi101112@users.noreply.github.com>
2025-06-21 19:54:54 +0530

    Delete backend directory

--- backend/backend/src/main/java/com/example/dropboxclone/repository/FileMetadataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dropboxclone.repository;
-
-    import com.example.dropboxclone.model.FileMetadata;
-    import org.springframework.data.jpa.repository.JpaRepository;
-
-    public interface FileMetadataRepository extends JpaRepository<FileMetadata, Long> {
-        FileMetadata findByFilename(String filename);
-    }
